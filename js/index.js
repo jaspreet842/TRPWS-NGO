d8cbec5a00af95aab6d369183d1b7dbff3d9d51e
jaspreet842 <55507569+jaspreet842@users.noreply.github.com>
2020-07-24 19:54:56 +0530

    Changes

--- js/index.js
+++ js/index.js
@@ -1,27 +1,29 @@
 let hamburger = document.querySelector('#hamBurger');
 let hamback = document.querySelector('#hamBack');
+let navlist = document.querySelector('#navList');
+let li = document.querySelectorAll('.navBtns');
+let logoimage = document.querySelector('.logoImg');
 
-hamburger.addEventListener('click', function() {
-    let navlist = document.querySelector('#navList');
-    let li = document.querySelectorAll('.navBtns');
-    let logoimage = document.querySelector('.logoImg');
+hamburger.addEventListener('click', function () {
     hamback.setAttribute('style', 'display: block;');
     logoimage.setAttribute('style', 'display: none;');
     navlist.setAttribute('style', 'display: flex; flex-direction: column; align-self: center; width: 100%; align-items: center; margin-top:1rem');
     hamburger.setAttribute('style', 'display: none;');
 });
-hamback.addEventListener('click', function() {
+
+hamback.addEventListener('click', function () {
+    console.log("It's working");
     navlist.setAttribute('style', 'display: none;')
     hamback.setAttribute('style', 'display:none;');
     hamburger.setAttribute('style', 'display: block;');
 })
 
 let btnApply = document.querySelector('.btnApply');
-btnApply.addEventListener('click', function() {
+btnApply.addEventListener('click', function () {
     let val = confirm('Do You want To Continue To Apply For Internship?');
-    if(val==false){
-        let a =  document.querySelector('#targetForm');
-        a.setAttribute('href', '#headingWhatWeDo');     
+    if (val == false) {
+        let a = document.querySelector('#targetForm');
+        a.setAttribute('href', '#headingWhatWeDo');
     }
 });
 
@@ -29,17 +31,17 @@ let btnReadMore = document.querySelectorAll('.btnReadMore');
 let contentReadMore = document.querySelectorAll('.contentReadMore');
 let btnReadLess = document.querySelectorAll('.btnReadLess');
 
-for(let i=0; i<btnReadMore.length; i++){
-    btnReadMore[i].addEventListener('click', function() {
+for (let i = 0; i < btnReadMore.length; i++) {
+    btnReadMore[i].addEventListener('click', function () {
 
         btnReadMore[i].setAttribute('style', 'display: none;');
         contentReadMore[i].setAttribute('style', 'display: block;');
         btnReadLess[i].setAttribute('style', 'display: block;');
 
-        btnReadLess[i].addEventListener('click', function(){
+        btnReadLess[i].addEventListener('click', function () {
             btnReadMore[i].setAttribute('style', 'display: block;');
             contentReadMore[i].setAttribute('style', 'display: none;');
-            btnReadLess[i].setAttribute('style', 'display: none;');            
+            btnReadLess[i].setAttribute('style', 'display: none;');
         });
     });
 }
